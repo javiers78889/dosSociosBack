463a095474095e2c012ad1e1cd5ac5b476fc15b5
javiers78889 <javiers78889@gmail.com>
2024-10-11 14:13:53 -0500

    con CORS

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import db from "./config/db"
 import express from 'express'
 import { router } from './routes/routes';
+import cors from 'cors'
 const connectDb = async () => {
     try {
         await db.authenticate()
@@ -16,8 +17,13 @@ const connectDb = async () => {
 connectDb()
 
 const app = express();
+app.use(cors({
+    origin: '*', // Permite solicitudes de todos los orígenes (ajustar según sea necesario)
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router)
 
 export default app
+
+
